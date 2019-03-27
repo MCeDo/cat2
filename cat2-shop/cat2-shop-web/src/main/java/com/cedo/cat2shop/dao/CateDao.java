809b61c167b1e0432948dc3ccd8f389c9016dc44
MCeDo <995011833@qq.com>
2019-03-27 21:54:48 +0800

    增加dao层方法

--- cat2-shop/cat2-shop-web/src/main/java/com/cedo/cat2shop/dao/CateDao.java
+++ cat2-shop/cat2-shop-web/src/main/java/com/cedo/cat2shop/dao/CateDao.java
@@ -3,6 +3,8 @@ package com.cedo.cat2shop.dao;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.cedo.cat2shop.model.Cate;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 
 /**
@@ -16,5 +18,11 @@ import org.springframework.stereotype.Component;
 @Component
 public interface CateDao extends BaseMapper<Cate> {
 
-
+    /**
+     * 查询类别名称
+     * @param cateId
+     * @return
+     */
+    @Select("select name from cate where id = #{cateId}")
+    String findCateNameById(@Param("cateId") Integer cateId);
 }
--- cat2-shop/cat2-shop-web/src/main/java/com/cedo/cat2shop/dao/ProductDao.java
+++ cat2-shop/cat2-shop-web/src/main/java/com/cedo/cat2shop/dao/ProductDao.java
@@ -1,9 +1,13 @@
 package com.cedo.cat2shop.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.cedo.cat2shop.model.Product;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 /**
  * 闲置物品
@@ -15,5 +19,6 @@ import org.springframework.stereotype.Component;
 @Mapper
 @Component
 public interface ProductDao extends BaseMapper<Product> {
-	
+
+    IPage<Product> selectPageWithImages(IPage page, Map<String, Object> params);
 }
